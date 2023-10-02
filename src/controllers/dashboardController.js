4746b3312b873a41c2edc363b79ae879b9e9d482
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2023-10-02 22:40:42 +0100

    added dashboardController.js route controller

---
+++ src/controllers/dashboardController.js
@@ -0,0 +1,72 @@
+const db = require('../utils/dbConfig');
+const util = require('util');
+
+module.exports = {
+    dashboard: async (req, res) => {
+        let activeUsers = 0;
+        let inactiveUsers = 0;
+        let activeCandidates = 0;
+        let inactiveCandidates = 0;
+        let activeElections = 0;
+        let inactiveElections = 0;
+
+        const query = "SELECT user_status, COUNT(*) AS users_count FROM users GROUP BY user_status";
+        const query2 = "SELECT candidate_status, COUNT(*) AS candidates_count FROM candidates GROUP BY candidate_status";
+        const query3 = "SELECT election_status, COUNT(*) AS elections_count FROM elections GROUP BY election_status";
+        const query4 = "SELECT COUNT(*) AS votes_count FROM votes";
+
+        const connection = await util.promisify(db.getConnection).bind(db)();
+
+        try
+        {
+            const userGroups = await util.promisify(connection.query).bind(connection)(query);
+            const candidateGroups = await util.promisify(connection.query).bind(connection)(query2);
+            const electionGroups = await util.promisify(connection.query).bind(connection)(query3);
+            const votes = await util.promisify(connection.query).bind(connection)(query4);
+
+            for(let i = 0; i < userGroups.length; i++)
+            {
+                const userGroup = userGroups[i];
+                userGroup.user_status === "Active" ? activeUsers += 1 : userGroup.user_status === "Inactive" ? inactiveUsers += 1 : null;
+            }
+
+            for(let i = 0; i < candidateGroups.length; i++)
+            {
+                const candidateGroup = candidateGroups[i];
+                candidateGroup.candidate_status === "Active" ? activeCandidates += 1 : candidateGroup.candidate_status === "Inactive" ? inactiveCandidates += 1 : null;
+            }
+
+            for(let i = 0; i < electionGroups.length; i++)
+            {
+                const electionGroup = electionGroups[i];
+                electionGroup.election_status === "Active" ? activeElections += 1 : electionGroup.election_status === "Inactive" ? inactiveElections += 1 : null;
+            }
+
+            const dashboard = {
+                activeUsers,
+                inactiveUsers,
+                activeCandidates,
+                inactiveCandidates,
+                activeElections,
+                inactiveElections,
+                total_votes:votes[0].votes_count
+            }
+
+            res.json({
+                error:false,
+                dashboard
+            })
+        }
+        catch(e)
+        {
+            res.json({
+                error:true,
+                message:e.message
+            })
+        }
+        finally
+        {
+            connection.release();
+        }
+    }
+}
