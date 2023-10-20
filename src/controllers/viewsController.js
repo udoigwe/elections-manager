4322b13a9113708770bcd27218e7c6ba062381b1
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2023-10-20 10:22:14 +0100

    Added more view route controllers

--- src/controllers/viewsController.js
+++ src/controllers/viewsController.js
@@ -11,8 +11,22 @@ module.exports = {
         res.render('index', { title:'Home', layout: './layouts/default'})
     },
 
-    /* Admin Routes */
+    /* Admin Controllers */
     adminDashboard: async (req, res) => {
         res.render('admin/index', { title:'Admin | Dashboard', layout: './layouts/admin'})
-    }
+    },
+    candidates: async (req, res) => {
+        res.render('admin/candidates', { title:'Admin | Candidates', layout: './layouts/admin' })
+    },
+    votes: async (req, res) => {
+        res.render('admin/votes', { title:'Admin | Votes', layout: './layouts/admin' })
+    },
+    users: async (req, res) => {
+        res.render('admin/users', { title:'Admin | Users', layout: './layouts/admin' })
+    },
+
+    /* Voter Controllers */
+    voterDashboard: async (req, res) => {
+        res.render('voter/index', { title:'Voter | Dashboard', layout: './layouts/voter'})
+    },
 }
